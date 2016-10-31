211ba065dd5114819f5f0b759e8b32cace9ad15e
edvalvitor <edval.vitor@gmail.com>
2016-10-31 18:34:00 -0200

    Script pós instalação debian

--- shell/debian.sh
+++ shell/debian.sh
@@ -9,7 +9,8 @@ apt install xorg alsa-{base,utils} xdg-user-dirs
 echo " "
 
 echo "-----Instalando pacote necessários para uso-----"
-apt install pcmanfm vim fonts-roboto obconf openbox lxappearance nitrogen dmenu chromium scrot screenfetch xarchiver ufw lxterminal git gimp
+#apt install pcmanfm vim fonts-roboto obconf openbox lxappearance nitrogen dmenu chromium scrot screenfetch xarchiver ufw lxterminal git gimp
+apt install --no-install-recommends gnome-{,core}
 
 echo "-----Baixando pacote de temas para Openbox-----"
 wget -c http://orig00.deviantart.net/bd03/f/2013/270/6/8/cathexis_suite_by_samirkahvedzic-d5g1i90.zip
