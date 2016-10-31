1754fc48291bf380e2e946871f4b0934f3469426
edvalvitor <edval.vitor@gmail.com>
2016-10-31 20:44:32 -0200

    Script debian

--- shell/debian.sh
+++ shell/debian.sh
@@ -10,10 +10,10 @@ echo " "
 
 echo "-----Instalando pacote necessários para uso-----"
 #apt install pcmanfm vim fonts-roboto obconf openbox lxappearance nitrogen dmenu chromium scrot screenfetch xarchiver ufw lxterminal git gimp
-apt install --no-install-recommends gnome-{,core}
+apt install --no-install-recommends gnome-{core,tweak-tool} gdm3 xarchiver ufw lxterminal screenfetch vim fonts-roboto
 
-echo "-----Baixando pacote de temas para Openbox-----"
-wget -c http://orig00.deviantart.net/bd03/f/2013/270/6/8/cathexis_suite_by_samirkahvedzic-d5g1i90.zip
+#echo "-----Baixando pacote de temas para Openbox-----"
+#wget -c http://orig00.deviantart.net/bd03/f/2013/270/6/8/cathexis_suite_by_samirkahvedzic-d5g1i90.zip
 
 echo "-----Criando pastas para temas e ícones-----"
 mkdir .themes
@@ -21,9 +21,9 @@ echo "Pasta de temas criada!"
 mkdir .icons
 echo "Pasta de ícones criada!"
 
-echo "-----Clondando repositórios de configurações-----"
-mkdir git
-cd git
-git clone https://github.com/edvalvitor/archlinux.git
+#echo "-----Clondando repositórios de configurações-----"
+#mkdir git
+#cd git
+#git clone https://github.com/edvalvitor/archlinux.git
 
 
